6e1686b168c7e5c626a4a67ad1fa644b1dc6e05a
Andres147XS <tabordaandres429@gmail.com>
2024-04-26 19:56:22 -0500

    finished quiz

--- caso-3/index.js
+++ caso-3/index.js
@@ -1,22 +1,17 @@
 import { money } from "../caso-1/index.js";
 
-// Función para convertir un número binario a decimal
 function binaryToDecimal(binary) {
     return parseInt(binary,2);
 }
 
-// Función para convertir un decimal a un caracter ASCII
 function decimalToChar(decimal) {
     return String.fromCharCode(decimal);
 }
 
-// Contraseña en números binarios
 const binaryPassword = "01110010_01101001_01110111_01101001";
 
-// Separar los números binarios por el guion bajo "_"
 const binaryArray = binaryPassword.split("_");
 
-// Convertir cada número binario a decimal y luego a caracter
 let password = "";
 binaryArray.forEach(binary => {
     const decimal = binaryToDecimal(binary);
@@ -24,7 +19,6 @@ binaryArray.forEach(binary => {
     password += character;
 });
 
-// Imprimir la contraseña
 console.log("Contraseña descifrada:", password);
 
 money = money - 50000
